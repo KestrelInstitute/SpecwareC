c42eb4a32e75e98b1e40724ea17c06aa01b5fee0
Edwin Westbrook <eddywestbrook@gmail.com>
2014-11-18 18:30:51 -0800

    finished defining specs as well as defining morphisms as model inclusion; started adding axioms to RecTypes; defined a more syntactic notion of super-specs

--- Spec_deprec.v
+++ Spec_deprec.v
@@ -14,94 +14,153 @@ Require Import Specware.Util.
 Definition Field : Set := string.
 Definition Field_dec : forall (f1 f2 : Field), {f1=f2} + {f1<>f2} := string_dec.
 
-Definition FieldList : Set := { flds : list Field | NoDup flds }.
 
-Definition map_NoDup (f : Field -> Field) (flds : FieldList) : FieldList :=
-  FIXME HERE.
-
-
+(*** Dependent record types ***)
 
 (* Dependent record types, indexed by their fields *)
-Inductive RecType : forall {flds : FieldList}, Type :=
+Inductive RecType : forall {flds : list Field}, Type :=
 | RecType_Nil : RecType (flds:=nil)
-| RecType_Cons f A {flds} :
+| RecType_Cons f {flds} A :
     (A -> RecType (flds:=flds)) -> RecType (flds:= f :: flds)
+| RecType_ConsAxiom f {flds} (P : Prop) :
+    (P -> RecType (flds:=flds)) -> RecType (flds:= f :: flds)
 .
 
-Fixpoint map_RecType (f : Field -> Field) {flds} (rectp : @RecType flds) :
-  @RecType (map_NoDup f flds) :=
-  FIXME HERE.
+(* Map a function over the fields of a RecType *)
+Fixpoint map_RecType (g : Field -> Field) {flds} (rectp : @RecType flds) :
+  @RecType (map g flds) :=
+  match rectp in @RecType flds return @RecType (map g flds) with
+    | RecType_Nil =>
+      RecType_Nil
+    | RecType_Cons f _ A rectp' =>
+      RecType_Cons (g f) A (fun a => map_RecType g (rectp' a))
+    | RecType_ConsAxiom f _ P rectp' =>
+      RecType_ConsAxiom (g f) P (fun pf => map_RecType g (rectp' pf))
+  end.
 
-(* Elements of dependent record types (FIXME: unnecessary?) *)
+(*** Subtypes ***)
+
+(* README: need subtype to be predicative in order to project along it! *)
 (*
-Inductive RecElem : forall {flds : list Field}, @RecType flds -> Type :=
-| RecElem_Nil : RecElem RecType_Nil
-| RecElem_Cons f A (a:A) {flds} (rectp: A -> @RecType flds)
-               (rest: RecElem (rectp a)) :
-    RecElem (RecType_Cons f A rectp)
+Inductive subtype : Type -> Type -> Prop :=
+| subtype_refl A : subtype A A
+| subtype_trans A1 A2 A3 (s1 : subtype A1 A2) (s2: subtype A2 A3) : subtype A1 A3
+| subtype_subsume A (P1 P2: A -> Prop) (sub: forall a, P1 a -> P2 a) :
+    subtype (sig P1) (sig P2)
+| subtype_restrict A (P: A -> Prop) : subtype (sig P) A
+| subtype_true A (P: A -> Prop) (truth: forall a, P a) :
+    subtype A (sig P)
 .
 *)
 
-(* Dependent records, independent of record type *)
-Inductive RecElem : forall {flds : list Field}, Type :=
-| RecElem_Nil : @RecElem nil
-| RecElem_Cons f A (a:A) {flds} (rest: @RecElem flds) :
-    @RecElem (f :: flds)
-.
 
-(* Helper lemma for lists *)
-Lemma In_inv_neq A (a b : A) l :
-  In a (b :: l) -> b <> a -> In a l.
-  intros in_ab neq; destruct (in_inv in_ab);
-  [ elimtype False; apply (neq H) | assumption ].
-Qed.
+(*** Models, aka records, aka heterogenous lists ***)
 
-(* Projecting a field in a RecElem *)
-Fixpoint RecElem_proj {flds} (elem: @RecElem flds) :
-  {f:Field | In f flds} -> { A : Type & A } :=
-  match elem in @RecElem flds return {f:Field | In f flds} -> { A : Type & A } with
-    | RecElem_Nil =>
-      (fun f => match in_nil (projT2 f) with end)
-    | RecElem_Cons f' A a _ elem' =>
-      (fun f =>
-         match Field_dec f' (projT1 f) with
-           | left _ => existT id A a
-           | right neq_pf =>
-             RecElem_proj elem' (existT _ (projT1 f)
-                                        (In_inv_neq _ _ _ _ (projT2 f) neq_pf))
-         end)
+Definition Any := { A : Type & A}.
+Definition Model := list (Field * Any).
+
+(* Project a field from a Model, returning unit if the field is not
+   there; i.e., all models intuitively map unused fields to unit *)
+Fixpoint Model_proj (model : Model) f : Any :=
+  match model with
+    | nil => existT id unit tt
+    | (f', any) :: model' =>
+      if Field_dec f' f then any else Model_proj model' f
   end.
 
+(* Project just the type component from a Model *)
+Definition Model_projT (model : Model) f : Type :=
+  projT1 (Model_proj model f).
 
-FIXME HERE:
-* define the restriction of a RecElem to a particular FieldList
-* loosen the IsElemOf to allow re-ordering and subsetting of fields
-* define morphisms s1 >=> s2 as:
-  forall models m of s2, applying a mapping to RecType s1 yields another s1'
-  such that the model m IsElemOf s1'
+(* Project just the object component from a Model *)
+Definition Model_projO (model : Model) f : Model_projT model f :=
+  projT2 (Model_proj model f).
 
-
-(* When a RecElem is an element of a RecType *)
-Inductive IsElemOf :
-  forall {flds : list Field}, @RecType flds -> @RecElem flds -> Prop :=
-| IsElemOf_Nil : IsElemOf RecType_Nil RecElem_Nil
-| IsElemOf_Cons f A a {flds} (rest_tp: A -> @RecType flds)
-                (rest_elem: @RecElem flds) :
-    IsElemOf (rest_tp a) rest_elem ->
-    IsElemOf (RecType_Cons f A rest_tp) (RecElem_Cons f A a rest_elem)
+(* When a Model is a model of a RecType *)
+Inductive IsModelOf_RT (m: Model) : forall {flds}, @RecType flds -> Prop :=
+| IsModelOf_RT_Nil : IsModelOf_RT m RecType_Nil
+| IsModelOf_RT_Cons f {flds}
+                    (rectp : Model_projT m f -> @RecType flds)
+                    (model_of : IsModelOf_RT m (rectp (Model_projO m f)))
+ : IsModelOf_RT m (RecType_Cons f (Model_projT m f) rectp)
+| IsModelOf_RT_ConsAxiom f {flds} (P:Prop) pf (rectp : P -> @RecType flds)
+                         (model_of : IsModelOf_RT m (rectp pf))
+ : IsModelOf_RT m (RecType_ConsAxiom f P rectp)
 .
 
-(* Bundling together RecElem with IsElemOf *)
-Definition RecElemOf {flds} (rectp: @RecType flds) :=
-  { elem: @RecElem flds | IsElemOf rectp elem }.
 
+(*** Specs ***)
 
-
-(* A Spec is a RecType with an arbitrary list of fields *)
-Record Spec :=
+(* A Spec is a RecType with an arbitrary field list *)
+Record Spec : Type :=
   {
     spec_fields : list Field;
-    spec_fields_nodups : NoDup spec_fields;
     spec_recType : @RecType spec_fields
   }.
+
+(* Mapping specs *)
+Definition mapSpec (g : Field -> Field) (spec: Spec) : Spec :=
+  {| spec_recType := map_RecType g (spec_recType spec) |}.
+
+(* When a Model is a model of a Spec *)
+Definition IsModelOf (m: Model) (spec: Spec) : Prop :=
+  IsModelOf_RT m (spec_recType spec).
+
+
+(*** Sub-specs and model inclusion ***)
+
+(* Model inclusion: when all models of spec1 are models of spec2 *)
+Definition Model_incl spec1 spec2 : Prop :=
+  forall m, IsModelOf m spec1 -> IsModelOf m spec2.
+
+(* Predicate for when a RecType contains a top-level field,type pair *)
+Inductive RecTypeContains (f:Field) (A:Type) :
+  forall {flds}, @RecType flds -> Prop :=
+| RecTypeContains_Base {flds} rectp :
+    RecTypeContains f A (@RecType_Cons f flds A rectp)
+| RecTypeContains_Cons f' {flds} A' rectp :
+    (forall a, RecTypeContains f A (rectp a)) ->
+    RecTypeContains f A (@RecType_Cons f' flds A' rectp)
+| RecTypeContains_ConsAxiom f' {flds} P rectp :
+    (forall pf, RecTypeContains f A (rectp pf)) ->
+    RecTypeContains f A (@RecType_ConsAxiom f' flds P rectp)
+.
+
+(* A super-spec is intuitively a spec with more fields and/or axioms *)
+Inductive SuperRecType {flds1} (rectp1: @RecType flds1) :
+  forall {flds2}, @RecType flds2 -> Prop :=
+| SuperRecType_Nil : SuperRecType rectp1 RecType_Nil
+| SuperRecType_Cons f {flds2} A rectp2 :
+    (forall a, @SuperRecType flds1 rectp1 flds2 (rectp2 a)) ->
+    RecTypeContains f A rectp1 ->
+    SuperRecType rectp1 (RecType_Cons f A rectp2)
+| SuperRecType_ConsAxiom f {flds2} (P:Prop) rectp2 :
+    (forall pf, @SuperRecType flds1 rectp1 flds2 (rectp2 pf)) ->
+    (forall model, IsModelOf_RT model rectp1 -> P) ->
+    SuperRecType rectp1 (RecType_ConsAxiom f P rectp2)
+.
+
+
+(*** Morphisms ***)
+
+(* A morphism is a function that maps a spec to a super-spec of another *)
+Definition IsMorphism (g: Field -> Field) spec1 spec2 : Prop :=
+  Model_incl spec2 (mapSpec g spec1).
+
+(* A morphism from spec1 to spec2 is a function plus a proof that the
+   function is a morphism *)
+Record Morphism spec1 spec2 :=
+  {
+    morphism_fun : Field -> Field;
+    morphism_pf : IsMorphism morphism_fun spec1 spec2
+  }.
+
+Notation "s1 >=> s2" := (Morphism s1 s2) (at level 70).
+
+
+(*** Transitivity of morphisms ***)
+
+Lemma map_Model_incl g spec1 spec2 :
+  Model_incl spec1 spec2 -> Model_incl (mapSpec g spec1) (mapSpec g spec2).
+  intro mincl.
 
