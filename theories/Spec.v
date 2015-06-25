37225ee2cfe0bea2831455814bce48bcef1e4f35
Edwin Westbrook <eddywestbrook@gmail.com>
2015-06-25 14:45:07 -0700

    trying to cons a field to the front of a morphism; ran into some difficulties

--- theories/Spec.v
+++ theories/Spec.v
@@ -26,6 +26,7 @@ Lemma Field_dec_neq f f' : f <> f' -> { neq : f <> f' | Field_dec f f' = right n
   exists n; reflexivity.
 Qed.
 
+
 (*** Field lists with no duplicates ***)
 
 Inductive flist : list Field -> Set :=
@@ -69,6 +70,20 @@ Lemma in_fl_uniq (f:Field) l (nd:NoDup l) (in1 in2 : In f l) : in1 = in2.
   f_equal; apply (IHl (NoDup_remove_1 [] l a nd)).
 Qed.
 
+(* Helper lemma for proving a field is not in a list *)
+Definition not_in_cons A (f f':A) l : f<>f' -> ~In f l -> ~In f (f'::l).
+  intros neq not_in i; destruct i.
+  apply neq; symmetry; assumption.
+  contradiction.
+Qed.
+
+(* Tactic for proving a field is not in a list *)
+Ltac solve_not_in_list :=
+  match goal with
+    | |- (~In ?f (@nil _)) => intro; assumption
+    | |- (~In ?f ?l) => apply not_in_cons; [ intro; discriminate | ]; try solve_not_in_list
+    | |- ?goal => fail "solve_not_in_list" goal
+  end.
 
 (* A field in a field list *)
 Definition Field_in {l} fl : Set := { f : Field | @in_fl f l fl }.
@@ -81,8 +96,13 @@ Definition Field_in_cons f' {l fl not_in} (f: Field_in fl) :
   Field_in (@fcons f' l not_in fl) :=
   mk_Field_in _ (proj1_sig f) (in_fl_cons (proj1_sig f) (proj2_sig f)).
 
+(* Remove the head of the field list that a Field_in is in *)
+Definition Field_in_tail {f' l not_in fl} (f: Field_in (@fcons f' l not_in fl))
+           (neq: proj1_sig f <> f') : Field_in fl :=
+  exist _ (proj1_sig f) (in_fl_tail _ neq (proj2_sig f)).
+
 (* Build a Field_in f (fcons f not_in fl) *)
-Definition Field_in_eq f {l} fl not_in : Field_in (@fcons f l not_in fl) :=
+Definition Field_in_eq f {l} not_in fl : Field_in (@fcons f l not_in fl) :=
   mk_Field_in _ f (in_fl_eq f not_in fl).
 
 (* Decidable equaltiy on Field_in *)
@@ -138,6 +158,15 @@ Lemma fmap_compose_composes l3 fl3 l2 fl2 l1 fl1 (m2 : FMap fl2 fl3) (m1 : FMap 
   = apply_fmap m2 (apply_fmap m1 f).
   reflexivity.
 Qed.
+
+(* Cons an op onto the source of a field map *)
+Definition fmap_cons {l1 fl1 l2 fl2} f not_in1 not_in2 (m: @FMap l1 fl1 l2 fl2) :
+  FMap (fcons f not_in1 fl1) (fcons f not_in2 fl2) :=
+  fun f' =>
+    match Field_dec (proj1_sig f') f with
+      | left _ => Field_in_eq f not_in2 fl2
+      | right neq => Field_in_cons f (m (Field_in_tail f' neq))
+    end.
 
 
 (*** Models ***)
@@ -214,6 +243,9 @@ Lemma unmap_model_compose {l3 fl3 l2 fl2} (m2: @FMap l2 fl2 l3 fl3)
   intro f; unfold unmap_model; rewrite fmap_compose_composes; reflexivity.
 Qed.
 
+(* The model_tail of unmap_model FIXME HERE *)
+
+
 
 (*** Specs ***)
 
@@ -222,12 +254,16 @@ Inductive SpecRepr : forall {l}, flist l -> Type :=
 (* The base case contains the names and types of the axioms *)
 | Spec_Axioms (axioms : list (Field * Prop)) : SpecRepr fnil
 (* Declared op: the rest of the spec can refer to the op *)
-| Spec_DeclOp f {l} flds not_in (T : Type) (rest : T -> SpecRepr flds)
+| Spec_DeclOp f {l flds} not_in (T : Type) (rest : T -> SpecRepr flds)
   : SpecRepr (@fcons f l not_in flds)
 (* Defined op: gives an element of the type *)
-| Spec_DefOp f {l} flds not_in (T : Type) (t : T) (rest : SpecRepr flds)
+| Spec_DefOp f {l flds} not_in (T : Type) (t : T) (rest : SpecRepr flds)
   : SpecRepr (@fcons f l not_in flds)
 .
+
+(* Make the field argument be parsed by Coq as a string *)
+Arguments Spec_DeclOp f%string l flds not_in T rest.
+Arguments Spec_DefOp f%string l flds not_in T t rest.
 
 (* A bundled type for specs and their fields *)
 Definition Spec : Type := {l:_ & {fl:_ & @SpecRepr l fl}}.
@@ -246,17 +282,17 @@ Definition conjoin_axioms (axioms : list (Field * Prop)) : Prop :=
 Fixpoint satisfies_specRepr {l flds} (spec: @SpecRepr l flds) : Model flds -> Prop :=
   match spec in @SpecRepr l flds return Model flds -> Prop with
     | Spec_Axioms axioms => fun _ => conjoin_axioms axioms
-    | Spec_DeclOp f flds' not_in T rest =>
+    | Spec_DeclOp f not_in T rest =>
       fun model =>
         exists (htim: has_type_in_model
-                        (Field_in_eq f flds' not_in) T model),
+                        (Field_in_eq f not_in _) T model),
           satisfies_specRepr
             (rest (model_proj _ T model htim))
             (model_tail model)
-    | Spec_DefOp f flds' not_in T t rest =>
+    | Spec_DefOp f not_in T t rest =>
       fun model =>
         (exists (htim: has_type_in_model
-                         (Field_in_eq f flds' not_in) T model),
+                         (Field_in_eq f not_in _) T model),
            model_proj _ T model htim = t) /\
         satisfies_specRepr rest (model_tail model)
   end.
@@ -272,35 +308,53 @@ Lemma satisfies_spec_equiv_models {l fl} (spec: @SpecRepr l fl) model1 model2 :
   intros equiv sats; induction spec.
   assumption.
   destruct sats as [htim sats'].
-  destruct (model_proj_equiv (Field_in_eq f flds not_in)
+  destruct (model_proj_equiv (Field_in_eq f not_in flds)
                              T model1 model2 htim equiv) as [htim' e].
   exists htim'; rewrite <- e.
   apply (H _ _ _ (model_tail_equiv _ _ equiv)). assumption.
   destruct sats as [H sats']; destruct H as [htim e].
   split.
-  destruct (model_proj_equiv (Field_in_eq f flds not_in)
+  destruct (model_proj_equiv (Field_in_eq f not_in flds)
                              T model1 model2 htim equiv) as [htim' e2].
   exists htim'; rewrite <- e2; assumption.
   apply (IHspec _ _ (model_tail_equiv _ _ equiv)).
   assumption.
 Qed.
 
+(* Helper notation for building specs (FIXME) *)
+(*
+Notation "'DeclOp' f T rest" := (Spec_DeclOp f%string $(solve_not_in_list)$ T rest)
+  (at level 0, f at level 0, T at level 0, rest at level 0).
+Notation "'DefOp' f T t rest" := (Spec_DefOp f $(solve_not_in_list)$ T rest) (at level 0).
+*)
+(*
+Notation "'Axioms f1 t1 ; .. ; fn tn'" :=
+  (Spec_Axioms (cons (f1, t1) .. (cons (fn, tn) nil)))
+  (at level 0).
+*)
+
+(* An example *)
+Program Definition spec_repr_example_1 :=
+  Spec_DeclOp "n" _ nat
+              (fun n => Spec_Axioms [("gt1"%string, n > 1)]).
+
 
 (*** Morphisms ***)
 
 (* m is a morphism from source to target iff it can unmap models of target into
 models of source *)
-Definition is_morphism (source target : Spec) m : Prop :=
+Definition is_morphism {l_s fl_s l_t fl_t}
+           (source: @SpecRepr l_s fl_s) (target: @SpecRepr l_t fl_t) m : Prop :=
   forall model,
-    satisfies_spec target model ->
-    satisfies_spec source (unmap_model m model).
+    satisfies_specRepr target model ->
+    satisfies_specRepr source (unmap_model m model).
 
 (* A morphism from source to target is an m that is_morphism *)
 Definition Morphism source target : Type :=
-  { m | is_morphism source target m }.
+  { m | is_morphism (specRepr source) (specRepr target) m }.
 
 (* The identity map on fl is a morphism from any spec on fl to itself *)
-Lemma is_morphism_id spec : is_morphism spec spec (fmap_id _).
+Lemma is_morphism_id spec : is_morphism (specRepr spec) (specRepr spec) (fmap_id _).
   unfold is_morphism; intros.
   apply (satisfies_spec_equiv_models _ _ _ (unmap_model_id _ _ model)).
   assumption.
@@ -312,8 +366,9 @@ Definition morph_id (spec:Spec) : Morphism spec spec :=
 
 (* Composing maps yields a morphism *)
 Lemma is_morphism_compose (s3 s2 s1: Spec) m2 m1 :
-  is_morphism s2 s3 m2 -> is_morphism s1 s2 m1 ->
-  is_morphism s1 s3 (fmap_compose m2 m1).
+  is_morphism (specRepr s2) (specRepr s3) m2 ->
+  is_morphism (specRepr s1) (specRepr s2) m1 ->
+  is_morphism (specRepr s1) (specRepr s3) (fmap_compose m2 m1).
   unfold is_morphism; intros ism2 ism1 model sats.
   apply (satisfies_spec_equiv_models _ _ _ (unmap_model_compose m2 m1 model)).
   apply ism1. apply ism2. assumption.
@@ -326,6 +381,40 @@ Definition morph_compose {s3 s2 s1}
   exist _ (fmap_compose (proj1_sig morph2) (proj1_sig morph1))
         (is_morphism_compose s3 s2 s1 (proj1_sig morph2) (proj1_sig morph1)
                              (proj2_sig morph2) (proj2_sig morph1)).
+
+(* A morphism on the tail of two specs extendeds to one on the full specs *)
+Lemma is_morphism_cons_decl f T {l_s fl_s l_t fl_t} not_in_s not_in_t
+           (source: T -> @SpecRepr l_s fl_s) (target: T -> @SpecRepr l_t fl_t)
+           (m: FMap fl_s fl_t) :
+  (forall t, is_morphism (source t) (target t) m) ->
+  is_morphism (Spec_DeclOp f not_in_s T source)
+              (Spec_DeclOp f not_in_t T target)
+              (fmap_cons f not_in_s not_in_t m).
+  intros ism model sats.
+  destruct sats as [htim sats].
+  assert (has_type_in_model (Field_in_eq f not_in_s fl_s) T
+                            (unmap_model (fmap_cons f not_in_s not_in_t m) model))
+    as htim'.
+  unfold has_type_in_model; unfold unmap_model; unfold apply_fmap; unfold fmap_cons;
+    unfold Field_in_eq at 1; unfold mk_Field_in; unfold proj1_sig.
+  destruct (Field_dec_eq f) as [e1 e2]; rewrite e2; assumption.
+  exists htim'.
+  unfold unmap_model; unfold model_proj.
+  
+
+  apply (ism).
+
+  unfold unmap_model; unfold model_proj.
+
+
+  exists htim.
+
+  econstructor. Check ?htim.
+
+
+  unfold satisfies_specRepr. unfold has_type_in_model. unfold unmap_model.
+
+  exists eq_refl.
 
 
 (*** Transformations ***)
