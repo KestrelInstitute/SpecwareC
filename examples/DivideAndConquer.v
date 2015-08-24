4c7ba139adef5d74329dcb5d85f1789bcff33187
Edwin Westbrook <eddywestbrook@gmail.com>
2015-08-24 14:55:36 -0700

    finished divide-and-conquer spec

--- examples/DivideAndConquer.v
+++ examples/DivideAndConquer.v
@@ -1,6 +1,6 @@
 
 Add LoadPath "../theories" as Specware.
-Require Import Specware.SpecwareC.
+Require Export Specware.SpecwareC.
 
 Require Import Recdef.
 
@@ -20,22 +20,26 @@ Spec Variable I : (D -> Prop).
 Spec Variable O : (D -> R -> Prop).
 
 (* The well-founded partial order on the inputs *)
-Spec Variable leq : (D -> D -> Prop).
-Spec Variable leq_wf : (well_founded leq).
+Spec Variable leq : (D -> D -> Prop) | (well_founded leq).
 
 (* The helper operations *)
 Spec Variable primitive : (D -> bool).
-Spec Variable decompose : (D -> D * D).
+Spec Variable decompose : (D -> D * D) | (forall d,
+                                            primitive d = false ->
+                                            leq (fst (decompose d)) d /\
+                                            leq (snd (decompose d)) d).
 Spec Variable compose : (R * R -> R).
 Spec Variable direct_solve : (D -> R).
 
 (* Soundness axioms *)
 Spec Axiom direct_solve_correct :
   (forall d, I d -> primitive d = true -> O d (direct_solve d)).
-Spec Axiom decompose_reduces1 :
-  (forall d, leq (fst (decompose d)) d).
-Spec Axiom decompose_reduces2 :
-  (forall d, leq (snd (decompose d)) d).
+Spec Axiom decompose_correct1 :
+  (forall d, I d -> primitive d = false ->
+             I (fst (decompose d))).
+Spec Axiom decompose_correct2 :
+  (forall d, I d -> primitive d = false ->
+             I (snd (decompose d))).
 Spec Axiom solve_soundness :
   (forall d0 z1 z2,
      O (fst (decompose d0)) z1 ->
@@ -61,22 +65,26 @@ Spec Variable I : (D -> Prop).
 Spec Variable O : (D -> R -> Prop).
 
 (* The well-founded partial order on the inputs *)
-Spec Variable leq : (D -> D -> Prop).
-Spec Variable leq_wf : (well_founded leq).
+Spec Variable leq : (D -> D -> Prop) | (well_founded leq).
 
 (* The helper operations *)
 Spec Variable primitive : (D -> bool).
-Spec Variable decompose : (D -> D * D).
+Spec Variable decompose : (D -> D * D) | (forall d,
+                                            primitive d = false ->
+                                            leq (fst (decompose d)) d /\
+                                            leq (snd (decompose d)) d).
 Spec Variable compose : (R * R -> R).
 Spec Variable direct_solve : (D -> R).
 
 (* Soundness axioms *)
 Spec Axiom direct_solve_correct :
   (forall d, I d -> primitive d = true -> O d (direct_solve d)).
-Spec Axiom decompose_reduces1 :
-  (forall d, leq (fst (decompose d)) d).
-Spec Axiom decompose_reduces2 :
-  (forall d, leq (snd (decompose d)) d).
+Spec Axiom decompose_correct1 :
+  (forall d, I d -> primitive d = false ->
+             I (fst (decompose d))).
+Spec Axiom decompose_correct2 :
+  (forall d, I d -> primitive d = false ->
+             I (snd (decompose d))).
 Spec Axiom solve_soundness :
   (forall d0 z1 z2,
      O (fst (decompose d0)) z1 ->
@@ -90,10 +98,27 @@ Function solve_def (d:D) {wf leq d} : R :=
   else
     let (d1, d2) := decompose d in
     compose (solve_def d1, solve_def d2).
+intros.
+replace d2 with (snd (decompose d)); [ | rewrite teq0; reflexivity ].
+apply (proj2 (decompose__proof _ teq)).
+intros.
+replace d1 with (fst (decompose d)); [ | rewrite teq0; reflexivity ].
+apply (proj1 (decompose__proof _ teq)).
+assumption.
+Defined.
 
 Spec Definition solve : (D -> R) := solve_def.
 
-Spec Axiom solve_correct :
-  (forall d, I d -> O d (solve d)).
+Spec Theorem solve_correct : (forall d, I d -> O d (solve d)).
+unfold solve; unfold def; intros.
+functional induction (solve_def d).
+apply direct_solve_correct; assumption.
+apply solve_soundness.
+assert (d1 = fst (decompose d)) as e1; [ rewrite e0; reflexivity | ].
+rewrite <- e1. apply IHr. rewrite e1. apply decompose_correct1; assumption.
+assert (d2 = snd (decompose d)) as e2; [ rewrite e0; reflexivity | ].
+rewrite <- e2. apply IHr0. rewrite e2. apply decompose_correct2; assumption.
+Qed.
 
 Spec End DivideAndConquer_impl.
+
