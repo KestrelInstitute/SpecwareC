b4abf8fa88cb633785a8ee30290b5781db99fe91
Edwin Westbrook <eddywestbrook@gmail.com>
2015-08-24 13:24:06 -0700

    added file LICENSE.txt, well as licensing-related text to the top of each source file

--- src/specware.ml4
+++ src/specware.ml4
@@ -1,3 +1,12 @@
+(* Copyright (c) 2015, Kestrel Institute
+All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the included LICENSE.txt file.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the LICENSE.txt for more details. *)
 
 DECLARE PLUGIN "specware"
 
--- theories/Spec.v
+++ theories/Spec.v
@@ -1,3 +1,13 @@
+(* Copyright (c) 2015, Kestrel Institute
+All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the included LICENSE.txt file.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the LICENSE.txt for more details. *)
+
 
 (*** Modeling specs and interpretations as Coq terms ***)
 
